cfd40193ba176f7a5440d0ba52fed67aa3d39140
Felipe Segovia Rodríguez <felipe.segovia.rod@gmail.com>
2023-05-03 14:57:05 -0400

    Se agreg CORS

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import dotenv from "dotenv";
 import TokenRouter from "./routes/token"
+import cors from "cors";
 
 dotenv.config({path: "./.env"})
 
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 app.use("/api/users", TokenRouter);
 
 const PORT = process.env.PORT || 3001;
