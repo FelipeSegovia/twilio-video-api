fbf67ee46659cefe997e85437042b6450483fd7c
Felipe Segovia Rodríguez <felipe.segovia.rod@gmail.com>
2023-06-13 16:55:12 -0400

    se pasan las variables en bruto

--- src/routes/token.ts
+++ src/routes/token.ts
@@ -11,12 +11,18 @@ const videoToken = (identity: string, room: string) => {
     } else {
         videoGrant = new AccessToken.VideoGrant();
     }
+    // const token = new AccessToken(
+    //         String(process.env.TWILIO_ACCOUNT_SID),
+    //         String(process.env.TWILIO_API_KEY),
+    //         String(process.env.TWILIO_API_SECRET),
+    //         { identity }
+    //     );
     const token = new AccessToken(
-            String(process.env.TWILIO_ACCOUNT_SID),
-            String(process.env.TWILIO_API_KEY),
-            String(process.env.TWILIO_API_SECRET),
-            { identity }
-        );
+        "AC1edee71fe1c02062b5d19c030ed0d296",
+        "6da1372f5412c24d963b17962558d64f",
+        "SKbcfd7a96683ec7c67433f7e919474ff4",
+        { identity }
+    );
     token.addGrant(videoGrant);
     return token;
 };
